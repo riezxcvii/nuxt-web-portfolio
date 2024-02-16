bb535456f31e6f8bcd63f452dabf4a5f3704c5a0
rieza-ix <rizmarezlo@gmail.com>
2024-02-16 18:02:55 +0800

    :lipstick: fix: change container type in skills

--- components/Skills.vue
+++ components/Skills.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-screen px-6">
+    <div class="min-w-screen px-6 mb-8">
         <!-- skill category title -->
         <h1 class="text-white font-black uppercase tracking-wider my-0 text-xl w-fit rounded-lg">
             {{ categoryName }}
@@ -8,7 +8,7 @@
         <!-- skill list -->
         <div>
             <ul
-                class="h-max py-4 mb-4 items-center mx-auto overflow-x-auto flex justify-start md:gap-6 gap-4">
+                class="h-max p-4 mb-4 items-center mx-auto flex-wrap flex justify-start md:gap-6 gap-4">
                 <li v-for="skill in skillCategory" :key="skill.id" class="bg-white w-max text-black rounded-md px-4 py-2">
                     <div class="flex w-full items-center justify-end h-full w-max">
                         <img :src="skill.logo" class="w-auto h-7 justify-between m-auto mr-2" :alt="skill.alt" />
