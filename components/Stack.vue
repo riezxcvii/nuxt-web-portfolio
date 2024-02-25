4d71d206385810fc32d45296ae3abf4fcc1eab34
rieza-ix <rizmarezlo@gmail.com>
2024-02-25 10:40:03 +0800

    :bug: fix: selection of skill list in tech stack

--- components/Stack.vue
+++ components/Stack.vue
@@ -2,10 +2,10 @@
     <div
         class="bg-transparent pt-4 pb-8 px-8 rounded-lg shadow-lg lg:h-[55vh] md:h-[90vh] h-max shadow-[#e6af2e] border-t-4 border-[#D7CEC2]">
         <!-- tech stack category -->
-        <h1 class="md:text-lg font-black uppercase tracking-wide my-4 mb-8 text-white">{{ title }}</h1>
+        <h1 class="md:text-lg font-black uppercase tracking-wide my-4 mb-8 text-white cursor-default select-none">{{ title }}</h1>
 
         <!-- tech stack list -->
-        <ul class="flex flex-wrap gap-2">
+        <ul class="flex flex-wrap gap-2 cursor-default select-none">
             <li v-for="stack in techStack" :key="stack.id"
                 class=" flex justify-center items-center bg-white md:px-3 px-2 py-2 rounded-lg">
                 <img :src="stack.logo" :alt="stack.alt" class="md:h-6 h-5 mr-1">
