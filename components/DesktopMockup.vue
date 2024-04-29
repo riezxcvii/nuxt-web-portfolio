22fedb1e4caf141a716fae572370f61f93b21436
rieza-ix <rizmarezlo@gmail.com>
2024-04-29 11:36:04 +0800

    :bug: fix: image not showing in desktop and mobile mockup

--- components/DesktopMockup.vue
+++ components/DesktopMockup.vue
@@ -3,7 +3,7 @@
         <div class="mx-auto border-black md:border-[10px] border-[8px] rounded-t-xl w-full h-fit bg-black">
             <!-- screen image -->
             <div class="rounded-lg overflow-hidden md:w-[18rem] w-[100%]">
-                <img :src="desktop" class="dark:hidden h-[13vh] md:h-full w-full rounded-lg my-0" :alt="alt" />
+                <img :src="desktop" class="h-[13vh] md:h-full w-full rounded-lg my-0" :alt="alt" />
             </div>
         </div>
         <div class="mx-auto bg-white shadow-xl rounded-b-xl h-[1.6vh] max-w-[301px] md:h-[20px] md:max-w-[512px]">
@@ -18,6 +18,4 @@ const props = defineProps({
     desktop: String,
     alt: String
 })
-
-const { desktop, alt } = props
 </script>
--- components/MobileMockup.vue
+++ components/MobileMockup.vue
@@ -4,7 +4,7 @@
             class="border-black md:border-[10px] border-[6px] md:rounded-[2rem] rounded-[1.1rem] md:h-[231px] h-[19.6vh] md:w-[130px] w-[4.8rem] shadow-xl">
             <div class="md:rounded-[1.3rem] rounded-[0.7rem] overflow-hidden w-full h-full bg-white">
                 <!-- screen image -->
-                <img :src="mobile" class="dark:hidden w-full h-full" :alt="alt" />
+                <img :src="mobile" class="w-full h-full" :alt="alt" />
             </div>
         </div>
     </div>
@@ -15,6 +15,4 @@ const props = defineProps({
     mobile: String,
     alt: String
 })
-
-const { mobile, alt } = props
 </script>
